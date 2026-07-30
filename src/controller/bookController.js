fdf36091c9226789c25a2a84daf6c915cfbb3f7d
MJunaid-Ahmad <mjunaidahmad7025@gmail.com>
2026-07-30 17:04:04 +0500

    Pagination

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -11,7 +11,6 @@ async function addBook(req, res, next) {
           if (!(category === undefined || category === ""))
             if (!(isbn === undefined || isbn === ""))
               if (!(publish === undefined || publish === "")) {
-
                 if (await bookModel.exists({ isbn }))
                   return res.status(409).json({
                     success: false,
@@ -34,14 +33,12 @@ async function addBook(req, res, next) {
                 });
               }
 
-    if(req.file)
-      deleteImage(req.file.path)
+    if (req.file) deleteImage(req.file.path);
 
     res.status(400).json({
-      success : false , 
-      message : "Invalid input data."
+      success: false,
+      message: "Invalid input data.",
     });
-
   } catch (err) {
     next(err);
   }
@@ -73,37 +70,33 @@ async function deleteBook(req, res, next) {
 
 async function updateBook(req, res, next) {
   try {
-    console.log(req.body)
+    console.log(req.body);
     let { title, author, category, isbn, publish, isAvailable } = req.body;
     let updatedData = {};
 
-  if (!(title === undefined || title === "")) 
-    updatedData.title = title;
+    if (!(title === undefined || title === "")) updatedData.title = title;
 
-  if (!(author === undefined || author === "")) 
-    updatedData.author = author;
+    if (!(author === undefined || author === "")) updatedData.author = author;
 
-  if (!(category === undefined || category === ""))
-    updatedData.category = category;
+    if (!(category === undefined || category === ""))
+      updatedData.category = category;
 
-  if (!(isbn === undefined || isbn === "")) 
-    updatedData.isbn = isbn;
+    if (!(isbn === undefined || isbn === "")) updatedData.isbn = isbn;
 
-  if (!(publish === undefined || publish === "")) 
-    updatedData.publish = publish;
+    if (!(publish === undefined || publish === ""))
+      updatedData.publish = publish;
 
-  if (!(isAvailable === undefined || isAvailable === ""))
-    updatedData.isAvailable = isAvailable;
+    if (!(isAvailable === undefined || isAvailable === ""))
+      updatedData.isAvailable = isAvailable;
 
-    let book = await bookModel.findOne({ isbn : req.params.isbn });
+    let book = await bookModel.findOne({ isbn: req.params.isbn });
     if (book) {
-
       if (req.file) {
         updatedData.coverImage = req.file.path;
         deleteImage(book.coverImage);
       }
 
-      await bookModel.findOneAndUpdate({ isbn : req.params.isbn }, updatedData, {
+      await bookModel.findOneAndUpdate({ isbn: req.params.isbn }, updatedData, {
         returnDocument: "after",
       });
 
@@ -111,8 +104,6 @@ async function updateBook(req, res, next) {
         success: true,
         message: "Book data Updated Successfully",
       });
-
-
     } else {
       return res.status(404).json({
         success: false,
@@ -122,30 +113,67 @@ async function updateBook(req, res, next) {
   } catch (err) {
     next(err);
   }
-
 }
 
 async function searchBooks(req, res, next) {
   try {
     let { title, author, category, publish } = req.query;
-    let filter = {};
 
-    if (title)  
-      filter.title = { $regex: title, $options: "i" };
-     
-    if (author)  
-      filter.author = { $regex: author, $options: "i"};
-     
-    if (category) 
-      filter.category = { $regex: category, $options: "i" };
-    
-    if (publish)  
-      filter.publish = Number(publish) ;
-       
-    let matchedBook = await bookModel.find(filter);
+    let page =
+      req.query.page === undefined || req.query.page === ""
+        ? 1
+        : Number(req.query.page) ;
+        
+    let limit =
+      req.query.limit === undefined || req.query.page === ""
+        ? 1
+        : Number(req.query.limit) ;
+
+    if (page <= 0 || isNaN(page))
+      return res.status(404).json({
+        success: false,
+        message: "Invalid Page Number",
+      });
+
+    if (limit <= 0 || isNaN(limit))
+      return res.status(404).json({
+        success: false,
+        message: "Invalid limit ",
+      });
+
+    let filter = {};
+    if (title) filter.title = { $regex: title, $options: "i" };
+
+    if (author) filter.author = { $regex: author, $options: "i" };
+
+    if (category) filter.category = { $regex: category, $options: "i" };
+
+    if (publish) filter.publish = Number(publish);
+
+    // Pagination Section
+
+    let totalBooks = await bookModel.countDocuments(filter);
+    let totalPages = Math.ceil(totalBooks / limit);
+    if (page > totalPages) page = totalPages;
+    if (limit > totalBooks) limit = totalBooks;
+    let skip = (page - 1) * limit;
+    let hasPrePage = false,
+      hasNextPage = true;
+    if (page > 1) hasPrePage = true;
+    if (page === totalPages) hasNextPage = false;
+
+    let matchedBook = await bookModel.find(filter).skip(skip).limit(limit);
     res.status(200).json({
       success: true,
       Books: matchedBook,
+      pagination: {
+        currentPage: page,
+        booksPerPage: limit,
+        totalBooks: totalBooks,
+        totalPages: totalPages,
+        hasNextPage: hasNextPage,
+        hasPreviousPage: hasPrePage,
+      },
     });
   } catch (err) {
     next(err);
@@ -155,13 +183,46 @@ async function searchBooks(req, res, next) {
 async function sortBooks(req, res, next) {
   try {
     let sort = req.query.sort;
-    if(req.query.sort)
-      sort = sort.toLowerCase()
+    if (req.query.sort) sort = sort.toLowerCase();
+
+    // Pagination section
+
+    let page =
+      req.query.page === undefined || req.query.page === ""
+        ? 1
+        : Number(req.query.page);
+    let limit =
+      req.query.limit === undefined || req.query.page === ""
+        ? 3
+        : Number(req.query.limit);
+    console.log(typeof(page))
+    if (page <= 0 || isNaN(page))
+      return res.status(404).json({
+        success: false,
+        message: "Invalid Page Number",
+      });
+
+    if (limit <= 0 || isNaN(limit))
+      return res.status(404).json({
+        success: false,
+        message: "Invalid limit ",
+      });
+
+    let totalBooks = await bookModel.countDocuments();
+    let totalPages = Math.ceil(totalBooks / limit);
+    if (page > totalPages) page = totalPages;
+    if (limit > totalBooks) limit = totalBooks;
+    let skip = (page - 1) * limit;
+    let hasPrePage = false,
+      hasNextPage = true;
+    if (page > 1) hasPrePage = true;
+    if (page === totalPages) hasNextPage = false;
+
     let sortedBooks;
     if (sort === "title") {
-      sortedBooks = await bookModel.find().sort({ title: 1 });
+      sortedBooks = await bookModel.find().sort({ title: 1 }).skip(skip).limit(limit);
     } else if (sort === "publish") {
-      sortedBooks = await bookModel.find().sort({ publish: 1 });
+      sortedBooks = await bookModel.find().sort({ publish: 1 }).skip(skip).limit(limit);
     } else {
       return res.status(400).json({
         success: false,
@@ -173,6 +234,14 @@ async function sortBooks(req, res, next) {
       success: true,
       Books: sortedBooks,
       message: "Books are sorted.",
+      pagination: {
+        currentPage: page,
+        booksPerPage: limit,
+        totalBooks: totalBooks,
+        totalPages: totalPages,
+        hasNextPage: hasNextPage,
+        hasPreviousPage: hasPrePage,
+      },
     });
   } catch (err) {
     next(err);
@@ -211,4 +280,3 @@ async function isAvailable(req, res, next) {
 }
 
 export { addBook, deleteBook, isAvailable, searchBooks, sortBooks, updateBook };
-
